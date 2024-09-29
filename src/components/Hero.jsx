1c840f8ae40488ab73e06dc85e9ff8d27b47e7e1
user <user@users-MacBook-Pro.local>
2024-09-29 20:54:56 +0800

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,8 +27,10 @@ const Hero=()=>{
                 </div>
                 
                 <div className="w-full lg:w-1/2 lg:p-8">
-                    <div className="flex justify-center">
-                        <motion.img initial={{x:100, opacity:0}}
+                    <div className="flex justify-center align-center">
+                        <motion.img
+                            className='w-3/4'
+                        initial={{x:100, opacity:0}}
                             animate={{x:0, opacity:1}}
                             transition={{
                                 duration:1, delay: 1.2
